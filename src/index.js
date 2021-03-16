50db4c8262ca43a49dbc62c2714583f660fedb2d
esfTunisie <projets@esftunisie.com>
2021-03-16 10:23:15 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import Store from './Redux/Store';
+import { persistor } from './Redux/Store'
+import { PersistGate } from "redux-persist/lib/integration/react";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PersistGate persistor={persistor}>
+      <Provider store={Store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
