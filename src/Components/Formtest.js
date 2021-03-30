de4164f4db3ff460eef7bbf0ff39ad4db89a4c2a
esfTunisie <projets@esftunisie.com>
2021-03-30 12:37:00 +0200

    up

--- src/Components/Formtest.js
+++ src/Components/Formtest.js
@@ -5,7 +5,7 @@ class Formtest extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-           value:null
+           
           
         };
     
@@ -13,18 +13,8 @@ class Formtest extends React.Component{
       }
 
       handleSubmit(){
-        const raw = JSON.stringify({"message":"amani "});
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-          };
-         
-  fetch("https://graph.facebook.com/105700664948995/feed?access_token=EAADtuD3xgEIBANRcLqNLPZCvEheVCkWZCiw2Q0HPwqR7ZCu0xDC4rQ16z0jJzVCW9eHSTdqrtL9bnBZClrdZBwdZAChfKqmm2tuIxzXLr7OTVdZBXlypsPgOT45OXEVEg5ttDWnA4oGhYKTaFq9ajQjJUZCUAU1Mz4FifFZAEfjDxFk9pwHUnfyTl9UzALCkHeE8ZD",
-  requestOptions).then(response => {
+    
+  fetch("https://graph.facebook.com/105700664948995/feed?access_token=EAADtuD3xgEIBANRcLqNLPZCvEheVCkWZCiw2Q0HPwqR7ZCu0xDC4rQ16z0jJzVCW9eHSTdqrtL9bnBZClrdZBwdZAChfKqmm2tuIxzXLr7OTVdZBXlypsPgOT45OXEVEg5ttDWnA4oGhYKTaFq9ajQjJUZCUAU1Mz4FifFZAEfjDxFk9pwHUnfyTl9UzALCkHeE8ZD").then(response => {
                 console.log(response);
             })
             .catch(error => {
@@ -47,13 +37,7 @@ class Formtest extends React.Component{
         console.log(this.state.value);
         return(
             <div>
-            <form  >
-            <label>
-              message :
-              <input type="text" name="message" value={this.state.value} onChange={this.handleChange}  />
-            </label>
-            <input type="submit" value="Envoyer" onClick={this.handleSubmit}  />
-          </form>
+
             </div>
         )
     }
--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -25,42 +25,21 @@ class TheHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      newCommand:{}
              };
   }
- handleClick (id){
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MTcwOTY4ODgsImV4cCI6MTYxNzEwMDQ4OCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiZmFyb3VrYnJAaWR2ZXkuY29tIn0.XvTLgk5ItEhzFGE7VS4TPQE-AsyJzpCkrbhhyJMELL_-GT6sbJvP0Fv6qAMHQJ6mjFsXobUahQbpaHbpLgP2HlzCeXZ2JpocOT9zvOHx_jn-94U1z_N7RTHCfLmNmepwTGqgrsjeYfHJJ-C2tQ7SvEn9cKAvdOh_ksjJHgDhFMrpm24AUBdj_t16JBScJG6J--oKvDZuh8sxur2X4QpnEnakx2v_MCAR_RCRKWSfo42oRU2V7eMMt8Wey7ylr1q_sHvLUgecK--q_T2suaybadg3mRUIotRQpiuPhY02_gSNTqrzeJQhRrRETnl_wEmjVEj43qFNI_E-43zzuEXxKA");
-  
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-  
-  fetch("http://localhost:8000/api/getNewCommande/"+id, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+ 
+ handleClick =async(id)=>{
 
-
-   /* fetch(apiURL+'/getNewCommande/api/'+id,{
-      'headers': {
-        'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MTcwOTY3MTIsImV4cCI6MTYxNzEwMDMxMiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiZmFyb3VrYnJAaWR2ZXkuY29tIn0.HQ-bbbhFPMutdNsEaitHb_ZtJnxg-Bsm5GRNsFdVwIqzW9DeH6gP3DTxQHtFBcFgOX5Ephtxzgof3qIqChNb1E2tA3PeyBAZbcYSH4c1XcHDjt_28Os_7T5UoeB-DYea7xxlocgfE2kxhUlzFAWWafq1pj4TKFNsrSxR07-_7pvVpkDaD3b-hQcFxEdRb0r3LwMFJL5Lm_8LMyUYGqTdJdOiVc_nqPFKqPcGEiBCE8Dm5bjKWEJ1YKU5tGla48NyW46qf9rZujlpu93_Q4SwREmqE9m_A5dkYCOYx_GsFD22PhDEAtC9a25r027goyTU9wujzTm7puhCPj2sad8Gbw'
-      }
-    })
-    .then(response => response.json()).then(data => this.setState({ newCommand:data }));*/
+  fetch(apiURL+'/api/getNewCommande/'+id)
+    .then(response => response.json()).then(data => this.setState({ newCommand:data }));
     const action = {type:"GET_NEW_COMMAND", value:this.state.newCommand}
       this.props.dispatch(action)
-      //this.handleClickCurrent(id);
+      this.handleClickCurrent(id);
   }
   handleClickCurrent (id){
     console.log('test',id);
-    fetch(apiURL+'/getCurrentCommande/api/'+id,{
-      'headers': {
-        'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MTcwOTY3MTIsImV4cCI6MTYxNzEwMDMxMiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiZmFyb3VrYnJAaWR2ZXkuY29tIn0.HQ-bbbhFPMutdNsEaitHb_ZtJnxg-Bsm5GRNsFdVwIqzW9DeH6gP3DTxQHtFBcFgOX5Ephtxzgof3qIqChNb1E2tA3PeyBAZbcYSH4c1XcHDjt_28Os_7T5UoeB-DYea7xxlocgfE2kxhUlzFAWWafq1pj4TKFNsrSxR07-_7pvVpkDaD3b-hQcFxEdRb0r3LwMFJL5Lm_8LMyUYGqTdJdOiVc_nqPFKqPcGEiBCE8Dm5bjKWEJ1YKU5tGla48NyW46qf9rZujlpu93_Q4SwREmqE9m_A5dkYCOYx_GsFD22PhDEAtC9a25r027goyTU9wujzTm7puhCPj2sad8Gbw'
-      }
-    })
+    fetch(apiURL+'/api/getCurrentCommande/'+id)
     .then(response=> response.json()).then(data=>this.setState({currentCommand:data}));
     const action = {type:"GET_CURRENT_COMMAND", value:this.state.currentCommand}
     this.props.dispatch(action)
